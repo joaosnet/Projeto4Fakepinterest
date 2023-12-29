8d396049df67f79480e357156ab4f7f593f99996
joaosnet <87316339+joaosnet@users.noreply.github.com>
2023-12-28 22:51:57 -0300

    Fix database configuration

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -5,12 +5,12 @@ from flask_bcrypt import Bcrypt
 from segredos import SECRET_KEY
 import os
 
-
 app = Flask(__name__)
 if os.getenv("DEBUG") == 0:
     link_banco = os.getenv("DATABASE_URL")
 else:
     link_banco = "sqlite:///comunidade.db"
+
 app.config["SQLALCHEMY_DATABASE_URI"] = link_banco
 app.config["SECRET_KEY"] = SECRET_KEY
 app.config["UPLOAD_FOLDER"] = "static/fotos_posts"
