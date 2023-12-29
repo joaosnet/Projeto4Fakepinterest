99eeb84d261d243cf4d173b573e8f29c1416946d
joaosnet <87316339+joaosnet@users.noreply.github.com>
2023-12-28 21:52:43 -0300

    Fix password encoding in login and account creation

--- fakepinterest/routes.py
+++ fakepinterest/routes.py
@@ -12,7 +12,7 @@ def homepage():
     form_login = FormLogin()
     if form_login.validate_on_submit():
         usuario = Usuario.query.filter_by(email=form_login.email.data).first()
-        if usuario and bcrypt.check_password_hash(usuario.senha, form_login.senha.data):
+        if usuario and bcrypt.check_password_hash(usuario.senha.enconde("utf-8"), form_login.senha.data):
             login_user(usuario, remember=True)
             return redirect(url_for("perfil", id_usuario=usuario.id))
     return render_template('homepage.html', form=form_login)
@@ -21,7 +21,7 @@ def homepage():
 def criarconta():
     form_criarconta = FormCriarConta()
     if form_criarconta.validate_on_submit():
-        senha = bcrypt.generate_password_hash(form_criarconta.senha.data)
+        senha = bcrypt.generate_password_hash(form_criarconta.senha.data).decode("utf-8")
         usuario = Usuario(username=form_criarconta.username.data,
                           email=form_criarconta.email.data, 
                           senha=senha)
